e1ae7e93beeef24b13b0b0ec72bd03d629dcbc2d
Prathamesh Gosavi <prathameshgosavi015@gmail.com>
2022-11-17 11:24:24 +0530

    first commit

---
+++ manage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+from numba import jit, cuda
+import numpy as np
+
+from timeit import default_timer as timer   
+
+
+    # normal function to run on cpu
+def func(a):                                
+    for i in range(500000000):
+        a[i]+= 1      
+  
+# function optimized to run on gpu 
+@jit(target_backend='cuda')                         
+def func2(a):
+    for i in range(500000000):
+        a[i]+= 1
+
+        
+if __name__=="__main__":
+    n = 500000000                         
+    a = np.ones(n, dtype = np.float64)
+      
+    start = timer()
+    func(a)
+    print("without GPU:", timer()-start)    
+      
+    start = timer()
+    func2(a)
+    print("with GPU:", timer()-start)
+
+
